fc7a61151f803d65b83089b70e05de001d70dda8
Aniruddha Kharve <kharveayash@gmail.com>
2025-07-10 17:09:28 +0530

    hii vpc variables

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "aws_region" {
+  description = "AWS region for subnets"
+  type        = string
+}

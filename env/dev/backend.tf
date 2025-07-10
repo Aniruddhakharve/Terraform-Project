ef6a07a632a69d4aa109066dc70aaaa0efe8ff33
Aniruddha Kharve <kharveayash@gmail.com>
2025-07-10 18:28:54 +0530

    hii

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-resume-backend"     # ✅ Your actual S3 bucket name
+    bucket         = "terraform-resume-backend"     # Your actual S3 bucket name
     key            = "env/dev/terraform.tfstate"    # Logical path to the state file
     region         = "ap-south-1"                   # Mumbai region
     dynamodb_table = "terraform-state-lock"         # Your DynamoDB table name
